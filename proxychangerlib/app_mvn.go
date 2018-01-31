22d0f36c7729c88e028ad827729c68c37402dfc1
Juan A. S <okelet@gmail.com>
2018-01-31 13:32:25 +0100

    Added Maven application

---
+++ proxychangerlib/app_mvn.go
@@ -0,0 +1,117 @@
+package proxychangerlib
+
+import (
+	"os"
+	"path"
+	"strconv"
+	"strings"
+
+	"github.com/beevik/etree"
+	"github.com/okelet/goutils"
+)
+
+// Register this application in the list of applications
+func init() {
+	RegisterProxifiedApplication(NewMavenProxySetter())
+}
+
+type MavenProxySetter struct {
+}
+
+func NewMavenProxySetter() *MavenProxySetter {
+	return &MavenProxySetter{}
+}
+
+func (a *MavenProxySetter) Apply(p *Proxy) *AppProxyChangeResult {
+
+	var err error
+	var password string
+
+	if p != nil {
+		password, err = p.GetPassword()
+		if err != nil {
+			return &AppProxyChangeResult{a, "", MyGettextv("Error getting the proxy password: %v", err)}
+		}
+	}
+
+	codePath, err := goutils.Which("mvn")
+	if err != nil {
+		return &AppProxyChangeResult{a, "", MyGettextv("Error checking if command %v exists: %v", "mvn", err)}
+	}
+
+	if codePath == "" {
+		return &AppProxyChangeResult{a, MyGettextv("Command %v not found", "mvn"), ""}
+	}
+
+	// Create dirs
+	mvnConfDirPath := path.Join(HOME_DIR, ".m2")
+	exists, err := goutils.DirExists(mvnConfDirPath)
+	if err != nil {
+		return &AppProxyChangeResult{a, "", MyGettextv("Error checking if directory %v exists: %v", mvnConfDirPath, err)}
+	}
+
+	if !exists {
+		err = os.MkdirAll(mvnConfDirPath, os.ModeDir)
+		if err != nil {
+			return &AppProxyChangeResult{a, "", MyGettextv("Error creating directory %v: %v", mvnConfDirPath, err)}
+		}
+	}
+
+	mvnConfFilePath := path.Join(mvnConfDirPath, "settings.xml")
+	doc := etree.NewDocument()
+	if exists {
+		err = doc.ReadFromFile(mvnConfFilePath)
+		if err != nil {
+			return &AppProxyChangeResult{a, "", MyGettextv("Error reading file %v: %v", mvnConfFilePath, err)}
+		}
+	}
+
+	settings := doc.SelectElement("settings")
+	settings.RemoveChild(settings.SelectElement("proxies"))
+	if p != nil {
+		proxies := settings.CreateElement("proxies")
+		for _, proxyType := range []string{"http", "https"} {
+			proxy := proxies.CreateElement("proxy")
+			proxy.CreateElement("id").SetText(proxyType)
+			proxy.CreateElement("protocol").SetText(proxyType)
+			proxy.CreateElement("active").SetText("true")
+			proxy.CreateElement("host").SetText(p.Address)
+			proxy.CreateElement("port").SetText(strconv.Itoa(p.Port))
+			if p.Username != "" {
+				proxy.CreateElement("username").SetText(p.Username)
+				if password != "" {
+					proxy.CreateElement("password").SetText(password)
+				}
+			}
+			if len(p.Exceptions) > 0 {
+				proxy.CreateElement("nonProxyHosts").SetText(strings.Join(p.Exceptions, "|"))
+			}
+		}
+	}
+
+	doc.Indent(2)
+
+	err = doc.WriteToFile(mvnConfFilePath)
+	if err != nil {
+		return &AppProxyChangeResult{a, "", MyGettextv("Error writing the file %v: %v", mvnConfFilePath, err)}
+	}
+
+	return &AppProxyChangeResult{a, "", ""}
+
+}
+
+func (a *MavenProxySetter) GetId() string {
+	return "mvn"
+}
+
+func (a *MavenProxySetter) GetSimpleName() string {
+	return MyGettextv("Maven")
+}
+
+func (a *MavenProxySetter) GetDescription() string {
+	return MyGettextv("Maven")
+}
+
+func (a *MavenProxySetter) GetHomepage() string {
+	return "https://github.com/okelet/proxychanger"
+}
